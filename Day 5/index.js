64efc4db9eee09b06b4a77c052d35a8689972b5b
Mohib Hussain <146108360+mohib990T@users.noreply.github.com>
2023-12-19 20:17:11 +0530

    Add files via upload

---
+++ Day 5/index.js
@@ -0,0 +1,49 @@
+// Comparison Operator
+
+let a = 5;      //Comparison between two values
+let b = 6;
+console.log(a==b); //false 
+
+let c = 5;      //Comparison between two values
+let d = '5';
+console.log(c === d); //false value is but datatype is different thats why we got false 
+
+let e = 10;
+let f = '10';
+console.log(e==f); //true
+
+// let x = 10;
+// let y = '10';
+// console.log(x !== y); //true
+
+let x = 10;
+let y = 10;
+console.log(x !== y); //true
+
+// let x = 10;
+// let y = 10;
+// console.log(x !== y); //false
+
+// let x = 10;
+// let y = 5;
+// console.log(x > y); //true
+
+// let x = 10;
+// let y = 5;
+// console.log(x >= y); //true
+
+// let x = 10;
+// let y = 5;
+// console.log(x += y); //15
+
+// let x = 5;
+// let y = 10;
+// console.log(x -= y); //-5
+
+// let x = 100;
+// let y = 20;
+// console.log(x % y); //0
+
+// let x = 100;
+// let y = 20;
+// console.log(x /= y); //5
