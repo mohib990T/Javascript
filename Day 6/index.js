74c916777632788ddff0644346321e315e9f05f4
Mohib Hussain <146108360+mohib990T@users.noreply.github.com>
2023-12-19 22:45:31 +0530

    js day 6

---
+++ Day 6/index.js
@@ -0,0 +1,54 @@
+// Increment and decrement ------->
+
+// a++   post increment
+// ++a   pre increment
+// a--   post decrement
+// --a   pre decrement
+
+// let a = 5;
+// console.log(a++ , a);
+
+// let a = 5;
+// console.log(++a , a);
+
+// let a = 5;
+// console.log(a-- , a);
+
+// let a = 5;
+// console.log(--a , a);
+
+// Logical Operator
+// &&, ||, !
+
+// console.log(5 > 10 && 2 < 5);
+// console.log(5 > 1 && 2 < 5);
+// console.log(5 > 10 || 2 < 5);
+// console.log(5 > 1 || 2 < 5);
+// console.log(!true);
+
+
+
+// let A = 2;
+// let B = 5;
+// let AddTwoNumbers = A+B;
+// console.log(AddTwoNumbers);
+
+
+// let a = 5;
+// let b = 3;
+// console.log(a<10 && a>b);
+
+// prompt  ---->
+
+// In prompt we get all the values, like number, string, boolean.
+// 
+// let age = prompt("Enter your age");
+// console.log(age);
+
+// let name = prompt("Enter your name");
+// console.log(name);
+
+let num1 = prompt("first num");
+let num2 = prompt("second num");
+let res = num1 + num2;
+console.log(res);
