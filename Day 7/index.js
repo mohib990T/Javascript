ea1f619d2abe5df11b46ce20ca01e0f15bc89bf8
Mohib Hussain <146108360+mohib990T@users.noreply.github.com>
2023-12-20 22:52:24 +0530

    js day-7

---
+++ Day 7/index.js
@@ -0,0 +1,17 @@
+// Conditional or ternary operator
+
+// let ter = (5 === 8) ? 'a' : 'b';
+// console.log(ter);
+
+
+// let num1 = 10;
+// let num2 = 20;
+// console.log(num1 === num2 ? "green" : "red");
+
+// let num1 = 10;
+// let num2 = 20;
+// console.log(num1 === num2 ? "green" : "red", num1 == num2 ? "black" : "yellow");
+
+let num1 = 10;
+let num2 = 20;
+console.log(num1 === num2, num1 == num2 ? "black" : "yellow");
